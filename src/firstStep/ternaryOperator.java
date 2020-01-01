7435e62fd4b0e54ec3ef236b32eaea1fc42e4d7c
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-01-01 12:05:51 +0600

    iniy

---
+++ src/firstStep/ternaryOperator.java
@@ -0,0 +1,17 @@
+package firstStep;
+
+public class ternaryOperator {
+    public static void main(String[] args) {
+
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+    }
+}

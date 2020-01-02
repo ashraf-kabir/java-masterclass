c98a0752adf9990fc86170b1a5069e5df7f50b25
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-01-02 16:35:43 +0600

    method overloading

---
+++ src/expCodebloksMethods/methodOverloading/Main.java
@@ -0,0 +1,55 @@
+package expCodebloksMethods.methodOverloading;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+        double centimeters = calcFeetAndInchesToCentimeters(7,5);
+        if (centimeters < 0.0) {
+            System.out.println("invalid parameters");
+        }
+
+        calcFeetAndInchesToCentimeters(157);
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if ((feet < 0) || (inches < 0) || (inches > 12)) {
+            System.out.println("invalid feet or inches parameters");
+            return -1;
+        }
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            System.out.println("invalid inches parameters");
+            return -1;
+        }
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches = " + feet + " feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player scored");
+        return 0;
+    }
+
+}

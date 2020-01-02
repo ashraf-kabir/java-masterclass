338babb798d10bce81886ea7b7dd0c694bfb3f28
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-01-02 17:09:17 +0600

    updated

--- src/expCodebloksMethods/secondsMinutes/test.java
+++ src/expCodebloksMethods/secondsMinutes/test.java
@@ -9,7 +9,24 @@ public class test {
 
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String hoursString = hours + "h";
+        if (hours < 10) {
+            hoursString = "0" + hoursString;
+        }
+
+        String minsString = remainingMinutes + "m";
+        if (remainingMinutes < 10) {
+            minsString = "0" + minsString;
+        }
+
+        String secondsString = seconds + "s";
+        if (seconds < 10) {
+            secondsString = "0" + secondsString;
+        }
+
+
+        return hoursString + " " + minsString + " " + secondsString;
     }
 
     public static String getDurationString(long seconds) {

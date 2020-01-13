fda421611bc6df9e95f6b4618f8b91dd6deaac67
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-01-13 22:42:40 +0600

    coposition

---
+++ src/oop/composition/Main.java
@@ -0,0 +1,18 @@
+package oop.composition;
+
+public class Main {
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+
+        Case theCase = new Case("220B", "Dell", "250", dimensions);
+
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+
+        Motherboard theMotherboard = new Motherboard("DG41RQ", "Asus", 4,5, "v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+
+        thePC.powerUp();
+
+    }
+}

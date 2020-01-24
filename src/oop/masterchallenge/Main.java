acb4aa27f2cdb5d52b3e5c4c309bca0bfd683829
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-01-24 15:25:12 +0600

    OOP completed

---
+++ src/oop/masterchallenge/Main.java
@@ -0,0 +1,20 @@
+package oop.masterchallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "white");
+        hamburger.addHamburgerAdditional1("Tomato", 0.27);
+        hamburger.addHamburgerAdditional2("Lettuce", 0.75);
+        hamburger.addHamburgerAdditional3("Cheese", 1.12);
+        System.out.println("Total burger price is " + hamburger.itemizedHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.66);
+        healthyBurger.addHamburgerAdditional1("Egg", 5.43);
+        healthyBurger.addHealthAdditional1("Lentils", 3.41);
+        System.out.println("Total Healthy burger price is " + healthyBurger.itemizedHamburger());
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+        deluxeBurger.addHamburgerAdditional1("Do not do this", 5.7);
+        deluxeBurger.itemizedHamburger();
+    }
+}

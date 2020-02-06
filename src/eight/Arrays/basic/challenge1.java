9a2d4b6ff0872d5f08c3f941ef8ebc17ba38d3f6
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-02-07 00:43:01 +0600

    updates

---
+++ src/eight/Arrays/basic/challenge1.java
@@ -0,0 +1,51 @@
+package eight.Arrays.basic;
+
+import java.util.Scanner;
+
+public class challenge1 {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] myInt = getIntegers(5);
+        int[] sorted = sortIntegers(myInt);
+        printArray(sorted);
+    }
+
+    public static int[] getIntegers(int capacity) {
+        int[] arr = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values:\r");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+}
